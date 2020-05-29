37986cf444f37d6c15d80f4fcfacad328c371958
Zainab Jimoh <jimohzainab4753@gmail.com>
2020-05-29 01:43:15 +0100

    Implemented auto capture of image after 3 seconds.

--- app/src/main/java/com/zennymorh/attendancetest/LoadingScreenFragment.kt
+++ app/src/main/java/com/zennymorh/attendancetest/LoadingScreenFragment.kt
@@ -3,22 +3,23 @@ package com.zennymorh.attendancetest
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.camera.core.Camera
-import androidx.camera.core.CameraSelector
-import androidx.camera.core.CameraX
-import androidx.camera.core.Preview
+import androidx.camera.core.*
 import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_loading_screen.*
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
@@ -27,9 +28,14 @@ import java.util.concurrent.Executors
  */
 class LoadingScreenFragment : Fragment() {
 
+    val endTime = 1000L
+    val startTime = 3000L
+    private var timer: CountDownTimer? = null
+
     private var preview: Preview? = null
     private var camera: Camera? = null
-
+    private var imageCapture: ImageCapture? = null
+    private lateinit var outputDirectory: File
     private lateinit var cameraExecutor: ExecutorService
 
     override fun onCreateView(
@@ -42,6 +48,8 @@ class LoadingScreenFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        outputDirectory = getOutputDirectory()
         cameraExecutor = Executors.newSingleThreadExecutor()
 
         // Request camera permissions
@@ -50,6 +58,16 @@ class LoadingScreenFragment : Fragment() {
         } else {
             requestPermissions(REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
         }
+
+        timer = object : CountDownTimer(startTime, endTime) {
+            override fun onTick(millisUntilFinished: Long) {
+                countdownText.text = ("" + millisUntilFinished/1000)
+            }
+
+            override fun onFinish() {
+                takePhoto()
+            }
+        }.start()
     }
 
     private fun startCamera() {
@@ -63,8 +81,12 @@ class LoadingScreenFragment : Fragment() {
             preview = Preview.Builder()
                 .build()
 
+            //Image capture code
+            imageCapture = ImageCapture.Builder()
+                .build()
+
             // Select back camera
-            val cameraSelector = CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build()
+            val cameraSelector = CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_FRONT).build()
 
             try {
                 // Unbind use cases before rebinding
@@ -72,7 +94,7 @@ class LoadingScreenFragment : Fragment() {
 
                 // Bind use cases to camera
                 camera = cameraProvider.bindToLifecycle(
-                    this, cameraSelector, preview)
+                    this, cameraSelector, preview, imageCapture)
                 preview?.setSurfaceProvider(previewView.createSurfaceProvider(camera?.cameraInfo))
             } catch(exc: Exception) {
                 Log.e(this.javaClass.simpleName, "Use case binding failed", exc)
@@ -81,10 +103,47 @@ class LoadingScreenFragment : Fragment() {
         }, ContextCompat.getMainExecutor(activity))
     }
 
+    private fun takePhoto() {
+        // Get a stable reference of the modifiable image capture use case
+        val imageCapture = imageCapture ?: return
+
+        // Create timestamped output file to hold the image
+        val photoFile = File(
+            outputDirectory,
+            SimpleDateFormat(FILENAME_FORMAT, Locale.US
+            ).format(System.currentTimeMillis()) + ".jpg")
+
+        // Create output options object which contains file + metadata
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+
+        // Setup image capture listener which is triggered after photo has
+        // been taken
+        imageCapture.takePicture(
+            outputOptions, ContextCompat.getMainExecutor(activity), object : ImageCapture.OnImageSavedCallback {
+                override fun onError(exc: ImageCaptureException) {
+                    Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
+                }
+
+                override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+                    val savedUri = Uri.fromFile(photoFile)
+                    val msg = "Photo capture succeeded: $savedUri"
+                    Toast.makeText(activity, msg, Toast.LENGTH_SHORT).show()
+                    Log.d(TAG, msg)
+                }
+            })
+    }
+
     private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
         ContextCompat.checkSelfPermission(
             requireContext(), it
         ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    fun getOutputDirectory(): File {
+        val mediaDir = context?.externalMediaDirs?.firstOrNull()?.let {
+            File(it, resources.getString(R.string.app_name)).apply { mkdirs() } }
+        return if (mediaDir != null && mediaDir.exists())
+            mediaDir else context?.applicationContext!!.filesDir
     }
 
     override fun onRequestPermissionsResult(
@@ -106,7 +165,11 @@ class LoadingScreenFragment : Fragment() {
     }
 
     companion object {
+        private const val TAG = "CameraXBasic"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
         private const val REQUEST_CODE_PERMISSIONS = 10
         private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
     }
+
+
 }
